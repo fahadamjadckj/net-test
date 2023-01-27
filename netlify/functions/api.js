9e5e0317859818054189a963be1af4752df64f23
martialdwarf <mohammadfahadamajd@gmail.com>
2023-01-27 11:30:36 +0500

    ss

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -19,41 +19,6 @@ export const handler = async () => {
       }),
     }
   }
-
-  const page = await browser.newPage();
-  await page.goto('http://lms.uaf.edu.pk/login/index.php');
-
-  const title = await page.title();
-
-  await page.evaluate(() => {
-    const input = document.querySelector('#REG');
-    input.focus();
-    input.value = '2020-ag-8322';
-
-    const button = document.querySelector('input[value=Result]');
-    button.click();
-  })
-
-  await page.waitForNavigation();
-
-  let data = await page.evaluate(() => {
-    let values = [];
-    let tableRows = document.querySelectorAll("tr");
-
-    tableRows.forEach((row) => {
-      let children = {};
-      let index = 0;
-      row.childNodes.forEach((child) => {
-        if (child.innerText != null) {
-          children[index] = child.innerText;
-          index++;
-        }
-      });
-      values.push(children);
-    });
-
-    return values;
-  });
     
     return {
       statusCode: 200,
