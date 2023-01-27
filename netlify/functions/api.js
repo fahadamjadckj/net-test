a4771a4cbfe0aeea21dbe14837a8e890095f50f5
martialdwarf <mohammadfahadamajd@gmail.com>
2023-01-27 15:41:22 +0500

    idk

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,14 +1,17 @@
-const puppeteer = require('puppeteer-core');
-const chromium = require('chrome-aws-lambda');
+const puppeteer = require("puppeteer-core");
+const chromium = require("@sparticuz/chromium");
 
 export const handler = async () => {
 
   const browser = await puppeteer.launch({
     args: chromium.args,
-    executablePath: await chromium.executablePath,
-    headless: true,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath(),
+    headless: chromium.headless,
+    ignoreHTTPSErrors: true,
   });
 
+  
   await browser.close();
  
     
